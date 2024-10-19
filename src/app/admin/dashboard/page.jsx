06d5fee7cbd72bb43d999e436113d7ebc0380e1e
dimpal001 <dimpaldas518@gmail.com>
2024-10-19 10:48:58 +0530

    resolve reference error in admin dashboard and create post page

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,93 +1,101 @@
-// 'use client'
+'use client'
+import dynamic from 'next/dynamic'
 
-// import Sidebar from '../../Components/components/Sidebar'
-// import Header from '../../Components/components/Header'
-// import UserManagement from '../../Components/components/UserManagement'
-// import PostManagement from '../../Components/components/PostManagement'
-// import CategoryManagement from '../../Components/components/CategoryManagement'
-// import { useEffect, useState } from 'react'
-// import { useRouter } from 'next/navigation'
-// import { useUserContext } from '@/app/context/UserContext'
-// import { enqueueSnackbar } from 'notistack'
-// import ReplyManagement from '../../Components/components/ReplyManagement'
-// import SubscriberManagement from '../../Components/components/SubscriberManagement'
-// import Loading from '@/app/Components/Loading'
-// import AboutUsManagement from '../../Components/components/AboutUsManagement'
-// import PrivacyPolicyManagement from '../../Components/components/PrivacyPlolicyManagement'
-// import TermsAndConditionManagement from '../../Components/components/TermsAndConditionManagement'
-// import HeroPostManagement from '../../Components/components/HeroPostManagement'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useUserContext } from '@/app/context/UserContext'
+import { enqueueSnackbar } from 'notistack'
 
-// const AdminDashboard = () => {
-//   const [activeSection, setActiveSection] = useState('posts')
-//   const [loading, setLoading] = useState(true)
-//   const [showDrawer, setShowDrawer] = useState(true)
-//   const router = useRouter()
-//   const { user } = useUserContext()
+const Sidebar = dynamic(() => import('../../Components/components/Sidebar'), {
+  ssr: false,
+})
+const Header = dynamic(() => import('../../Components/components/Header'), {
+  ssr: false,
+})
+const UserManagement = dynamic(
+  () => import('../../Components/components/UserManagement'),
+  { ssr: false }
+)
+const PostManagement = dynamic(
+  () => import('../../Components/components/PostManagement'),
+  { ssr: false }
+)
+const CategoryManagement = dynamic(
+  () => import('../../Components/components/CategoryManagement'),
+  { ssr: false }
+)
+const ReplyManagement = dynamic(
+  () => import('../../Components/components/ReplyManagement'),
+  { ssr: false }
+)
+const SubscriberManagement = dynamic(
+  () => import('../../Components/components/SubscriberManagement'),
+  { ssr: false }
+)
+const AboutUsManagement = dynamic(
+  () => import('../../Components/components/AboutUsManagement'),
+  { ssr: false }
+)
+const PrivacyPolicyManagement = dynamic(
+  () => import('../../Components/components/PrivacyPlolicyManagement'),
+  { ssr: false }
+)
+const TermsAndConditionManagement = dynamic(
+  () => import('../../Components/components/TermsAndConditionManagement'),
+  { ssr: false }
+)
+const HeroPostManagement = dynamic(
+  () => import('../../Components/components/HeroPostManagement'),
+  { ssr: false }
+)
 
-//   useEffect(() => {
-//     // Check if the code is running in the browser
-//     if (typeof window !== 'undefined') {
-//       if (user) {
-//         if (user.role === 'admin') {
-//           console.log('Admin user detected.')
-//           setLoading(false)
-//         } else {
-//           console.log('Guest user detected.')
-//           router.push('/')
-//         }
-//       } else {
-//         console.log('No user found. Redirecting.')
-//         router.push('/')
-//         enqueueSnackbar('You are not allowed to enter this page.', {
-//           variant: 'error',
-//         })
-//       }
-//     }
-//   }, [user, router])
+const AdminDashboard = () => {
+  const [activeSection, setActiveSection] = useState('posts')
+  const [loading, setLoading] = useState(true)
+  const [showDrawer, setShowDrawer] = useState(true)
+  const router = useRouter()
+  const { user } = useUserContext()
 
-//   if (loading) {
-//     return <Loading />
-//   }
+  useEffect(() => {
+    if (!user) {
+      enqueueSnackbar('You are not allowed to enter this page.', {
+        variant: 'error',
+      })
+      router.push('/')
+    }
+  }, [router])
 
-//   return (
-//     <div className='flex h-screen bg-gray-100'>
-//       {/* Sidebar */}
-//       {showDrawer && (
-//         <Sidebar
-//           activeSection={activeSection}
-//           setActiveSection={setActiveSection}
-//         />
-//       )}
+  return (
+    <div className='flex h-screen bg-gray-100'>
+      {/* Sidebar */}
+      {showDrawer && (
+        <Sidebar
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+        />
+      )}
 
-//       {/* Main content */}
-//       <div className='flex-1 flex flex-col'>
-//         <Header setShowDrawer={setShowDrawer} showDrawer={showDrawer} />
+      {/* Main content */}
+      <div className='flex-1 flex flex-col'>
+        <Header setShowDrawer={setShowDrawer} showDrawer={showDrawer} />
 
-//         {/* Dynamic content */}
-//         <main className='p-4 overflow-y-scroll'>
-//           {activeSection === 'users' && <UserManagement />}
-//           {activeSection === 'posts' && <PostManagement />}
-//           {activeSection === 'categories' && <CategoryManagement />}
-//           {activeSection === 'replies' && <ReplyManagement />}
-//           {activeSection === 'subscribers' && <SubscriberManagement />}
-//           {activeSection === 'about-us' && <AboutUsManagement />}
-//           {activeSection === 'privacy-policy' && <PrivacyPolicyManagement />}
-//           {activeSection === 'terms-and-condition' && (
-//             <TermsAndConditionManagement />
-//           )}
-//           {activeSection === 'hero-posts' && <HeroPostManagement />}
-//         </main>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default AdminDashboard
-
-import React from 'react'
-
-const page = () => {
-  return <div>page</div>
+        {/* Dynamic content */}
+        <main className='p-4 overflow-y-scroll'>
+          {activeSection === 'users' && <UserManagement />}
+          {activeSection === 'posts' && <PostManagement />}
+          {activeSection === 'categories' && <CategoryManagement />}
+          {activeSection === 'replies' && <ReplyManagement />}
+          {activeSection === 'subscribers' && <SubscriberManagement />}
+          {activeSection === 'about-us' && <AboutUsManagement />}
+          {activeSection === 'privacy-policy' && <PrivacyPolicyManagement />}
+          {activeSection === 'terms-and-condition' && (
+            <TermsAndConditionManagement />
+          )}
+          {activeSection === 'hero-posts' && <HeroPostManagement />}
+        </main>
+      </div>
+    </div>
+  )
 }
 
-export default page
+export default AdminDashboard
