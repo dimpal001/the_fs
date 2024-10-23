db5513839f48dd315f603bd988f457993de9cd79
dimpal001 <dimpaldas518@gmail.com>
2024-10-23 11:24:27 +0530

    resolve internal and external links problem

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -3,6 +3,7 @@ import SubscribeCard from './SubscribeCard'
 import {
   FacebookIcon,
   InstagramIcon,
+  PinterestIcon,
   TwitterIcon,
   WhatsappIcon,
 } from 'next-share'
@@ -12,62 +13,122 @@ const Footer = () => {
     <footer className='bg-gray-800 mt-10 text-white py-10'>
       <div className='container mx-auto px-6'>
         <div className='flex flex-col gap-5 md:flex-row justify-between'>
-          <div className='mb-6 md:mb-0'>
+          {/* About Us Section */}
+          <div className='lg:w-1/2 mb-6 md:mb-0'>
             <h4 className='text-lg font-semibold mb-4'>About Us</h4>
             <p className='text-gray-400'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              quisquam qui nisi quod cupiditate quasi.
+              We provide the latest in fashion trends and tips. Stay up-to-date
+              with our blog for new fashion advice and inspirations.
             </p>
             <div className='w-full flex mt-3'>
               <SubscribeCard isSmall={true} />
             </div>
           </div>
-          <div className='mb-6 md:mb-0'>
+
+          {/* Quick Links Section */}
+          <div className='lg:w-1/3 lg:ps-20 mb-6 md:mb-0'>
             <h5 className='text-lg font-semibold mb-4'>Quick Links</h5>
             <ul className='space-y-2'>
               <li>
-                <Link href={'/'}>Home</Link>
-              </li>
-              {/* <li>
-                <Link href={'/'}></Link>
-                <a href='#' className='hover:text-gray-400'>
-                  Services
-                </a>
-              </li> */}
-              <li>
-                <Link href={'/about'}>About</Link>
+                <Link href='/' className='hover:text-gray-400'>
+                  Home
+                </Link>
               </li>
               <li>
-                <Link href={'/privacy-policy'}>Privacy Policy</Link>
+                <Link href='/about' className='hover:text-gray-400'>
+                  About Us
+                </Link>
               </li>
               <li>
-                <Link href={'/terms-and-condition'}>Terms & Condition</Link>
+                <Link
+                  target='_blank'
+                  href='www.clothes2wear.in'
+                  className='hover:text-gray-400'
+                >
+                  Shop
+                </Link>
               </li>
               <li>
-                <Link href={'/contact'}>Contact</Link>
+                <Link href='/privacy-policy' className='hover:text-gray-400'>
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href='/terms-and-condition'
+                  className='hover:text-gray-400'
+                >
+                  Terms & Condition
+                </Link>
+              </li>
+              <li>
+                <Link href='/contact' className='hover:text-gray-400'>
+                  Contact Us
+                </Link>
+              </li>
+              <li>
+                <Link href='/faq' className='hover:text-gray-400'>
+                  FAQ
+                </Link>
               </li>
             </ul>
           </div>
-          <div className='mb-6 md:mb-0'>
+
+          {/* Follow Us Section */}
+          <div className='lg:w-1/3 mb-6 md:mb-0'>
             <h6 className='text-lg font-semibold mb-4'>Follow Us</h6>
             <div className='flex space-x-4'>
-              <a target='_blank' href={'https://www.instagram.com'}>
+              <a
+                target='_blank'
+                href='https://www.instagram.com'
+                aria-label='Follow us on Instagram'
+                rel='noopener noreferrer'
+                className='hover:text-gray-400'
+              >
                 <InstagramIcon round size={45} />
               </a>
-              <a target='_blank' href={'https://www.facebook.com'}>
+              <a
+                target='_blank'
+                href='https://www.facebook.com'
+                aria-label='Follow us on Facebook'
+                rel='noopener noreferrer'
+                className='hover:text-gray-400'
+              >
                 <FacebookIcon round size={45} />
               </a>
-              <a target='_blank' href={'https://www.whatsapp.com'}>
+              <a
+                target='_blank'
+                href='https://www.whatsapp.com'
+                aria-label='Follow us on WhatsApp'
+                rel='noopener noreferrer'
+                className='hover:text-gray-400'
+              >
                 <WhatsappIcon round size={45} />
               </a>
-
-              <a target='_blank' href={'https://www.x.com'}>
+              <a
+                target='_blank'
+                href='https://www.x.com'
+                aria-label='Follow us on Twitter'
+                rel='noopener noreferrer'
+                className='hover:text-gray-400'
+              >
                 <TwitterIcon round size={45} />
+              </a>
+              <a
+                target='_blank'
+                href='https://in.pinterest.com/'
+                aria-label='Follow us on Twitter'
+                rel='noopener noreferrer'
+                className='hover:text-gray-400'
+              >
+                <PinterestIcon round size={45} />
               </a>
             </div>
           </div>
         </div>
       </div>
+
+      {/* Footer Bottom */}
       <div className='border-t border-gray-700 mt-8 pt-4'>
         <div className='container mx-auto text-center'>
           <p className='text-sm text-gray-400'>
