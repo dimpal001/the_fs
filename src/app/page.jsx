f7a656b0c0754c3d2060fdea83def0fa058c1e4b
dimpal001 <dimpaldas518@gmail.com>
2024-11-08 12:27:18 +0530

    update sitemap

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import axios from 'axios'
-import HomePage from './Homepage'
+import dynamic from 'next/dynamic'
 
-// Fetching data
+// Conditionally load the HomePage component only on the client side
+const HomePage = dynamic(() => import('./Homepage'), { ssr: false })
+
 const handleFetchHeroPosts = async () => {
   try {
     const response = await axios.get(
@@ -28,15 +30,16 @@ const handleFetchLatestPosts = async () => {
     )
     return response.data
   } catch (error) {
-  } finally {
+    console.error('Error fetching latest posts:', error)
+    return null
   }
 }
 
 const Page = async () => {
-  // Fetch data
   const heroPosts = await handleFetchHeroPosts()
   const latestPosts = await handleFetchLatestPosts()
 
+  // Pass data to client-only HomePage
   return <HomePage heroPost={heroPosts} latestPost={latestPosts} />
 }
 
