e0d328c182a1ca53acb215e18c2a473988119834
dimpal001 <dimpaldas518@gmail.com>
2024-11-08 18:07:50 +0530

    resolve post managemenet issue

--- src/app/Components/components/PostManagement.jsx
+++ src/app/Components/components/PostManagement.jsx
@@ -1,16 +1,26 @@
 import axios from 'axios'
+import { ChevronDown } from 'lucide-react'
 import { enqueueSnackbar } from 'notistack'
 import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { DynamicMenu } from '../DynamicMenu'
+import RejectPostModal from './RejectPostModal'
+import ReviewPostModal from './ReviewPostModal'
+import ConfirmModal from './ConfirmModal'
 import Input from '@/app/Components/Input'
 import Loading from '@/app/Components/Loading'
 import Paggination from './Paggination'
+import { useRouter } from 'next/navigation'
+import { DynamicMenu } from '../DynamicMenu'
 
 const PostManagement = () => {
   const [loading, setLoading] = useState(true)
   const [posts, setPosts] = useState([])
   const [filteredPosts, setFilteredPosts] = useState([])
+  const [isApprovingId, setIsApprovingId] = useState(null)
+  const [isRejectModalOpen, setIsRejectModalOpen] = useState(false)
+  const [isReviewModalOpen, setIsReviewModalOpen] = useState(false)
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false)
+  const [confirmAction, setConfirmAction] = useState('')
+  const [selectedPost, setSelectedPost] = useState({})
   const [filterStatus, setFilterStatus] = useState('All')
   const [filterCategory, setFilterCategory] = useState('All')
   const [searchTerm, setSearchTerm] = useState('')
@@ -20,7 +30,6 @@ const PostManagement = () => {
   const [categories, setCategories] = useState(null)
   const router = useRouter()
 
-  // Unified function to fetch posts with all filters
   const handleFetchPosts = async (page = 1) => {
     setLoading(true)
 
@@ -52,13 +61,77 @@ const PostManagement = () => {
     }
   }
 
+  // Filter posts based on status, category, and search term
+  const handleFilterPosts = () => {
+    let filtered = posts
+
+    // Filter by status
+    if (filterStatus !== 'All') {
+      filtered = filtered.filter(
+        (post) => post.status === filterStatus.toLowerCase()
+      )
+    }
+
+    // Filter by category
+    if (filterCategory !== 'All') {
+      const categoryId = categories.find(
+        (cat) => cat.slug === filterCategory
+      )?.slug
+
+      console.log(categoryId)
+
+      if (categoryId) {
+        filtered = filtered.filter((post) =>
+          post.category_ids.includes(categoryId)
+        )
+      }
+    }
+
+    // Filter by search term
+    if (searchTerm) {
+      filtered = filtered.filter((post) =>
+        post.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    }
+
+    setFilteredPosts(filtered)
+  }
+
+  const handleApprovePost = async (post) => {
+    if (post.status === 'approve') {
+      return null
+    }
+    setIsApprovingId(post.id)
+    try {
+      const response = await axios.patch(
+        `/api/posts/${post.id}`,
+        {
+          status: 'approve',
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      )
+      setPosts((prevPosts) =>
+        prevPosts.map((p) =>
+          p.id === post.id ? { ...p, status: 'approve' } : p
+        )
+      )
+      enqueueSnackbar(response.data.message, { variant: 'success' })
+    } catch (error) {
+      enqueueSnackbar(error.response.data.message, { variant: 'error' })
+    } finally {
+      setIsApprovingId(null)
+    }
+  }
+
   const handleFetchCategories = async () => {
     try {
       const response = await axios.get('/api/admin/category')
       setCategories(response.data)
-    } catch (error) {
-      enqueueSnackbar('Failed to fetch categories', { variant: 'error' })
-    }
+    } catch (error) {}
   }
 
   const handleNextPage = () => {
@@ -82,15 +155,75 @@ const PostManagement = () => {
     handleFetchCategories()
   }, [])
 
+  // useEffect(() => {
+  //   handleFilterPosts(currentPage)
+  // }, [filterStatus, filterCategory, searchTerm, posts])
+
   useEffect(() => {
     handleFetchPosts(currentPage)
   }, [searchTerm, filterStatus, filterCategory])
+
+  const handleReviewModal = (post) => {
+    setSelectedPost(post)
+    setIsReviewModalOpen(true)
+  }
+
+  const handleDeactive = (post) => {
+    setSelectedPost(post)
+    setConfirmAction('deactivate')
+    setIsConfirmModalOpen(true)
+  }
+
+  const handleDelete = (post) => {
+    setSelectedPost(post)
+    setConfirmAction('delete')
+    setIsConfirmModalOpen(true)
+  }
+
+  const handleConfirmAction = async () => {
+    if (confirmAction === 'deactivate') {
+      setConfirmAction(false)
+      try {
+        const response = await axios.patch(`/api/posts/${selectedPost.id}`, {
+          status: 'deactivated',
+        })
+        setPosts((prevPosts) =>
+          prevPosts.map((p) =>
+            p.id === selectedPost.id ? { ...p, status: 'deactivated' } : p
+          )
+        )
+        enqueueSnackbar('Post deactivated successfully', { variant: 'success' })
+      } catch (error) {
+        enqueueSnackbar('Error deactivating post', { variant: 'error' })
+      } finally {
+        setIsConfirmModalOpen(false)
+      }
+    } else if (confirmAction === 'delete') {
+      setConfirmAction(false)
+      try {
+        await axios.delete(`/api/posts/${selectedPost.id}`)
+        setPosts((prevPosts) =>
+          prevPosts.filter((p) => p.id !== selectedPost.id)
+        )
+        enqueueSnackbar('Post deleted successfully', { variant: 'success' })
+      } catch (error) {
+        enqueueSnackbar('Error deleting post', { variant: 'error' })
+      } finally {
+        setIsConfirmModalOpen(false)
+      }
+    }
+    setIsConfirmModalOpen(false)
+  }
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     <div>
       <div className='flex justify-between'>
         <h2 className='text-2xl font-bold mb-4'>Manage Posts</h2>
-        {totalPosts && <p className='text-sm'>Total Posts: {totalPosts}</p>}
+        {totalPosts && <p className='text-sm'>Total Posts : {totalPosts}</p>}
       </div>
 
       <div className='flex justify-between mb-4'>
@@ -109,6 +242,7 @@ const PostManagement = () => {
             <option value='All'>All</option>
             <option value='Approve'>Published</option>
             <option value='Pending'>Pending</option>
+            {/* <option value='Reject'>Rejected</option> */}
             <option value='Deactivated'>Deactivated</option>
           </select>
           {categories && categories.length > 0 && (
@@ -127,7 +261,6 @@ const PostManagement = () => {
           )}
         </div>
       </div>
-
       <p className='pb-2 px-2 text-gray-600'>
         {filteredPosts.length} Results found
       </p>
@@ -166,27 +299,64 @@ const PostManagement = () => {
                     button={
                       <button className='flex items-center gap-2 p-1 px-4 border border-dotted'>
                         Action
+                        <ChevronDown />
                       </button>
                     }
                   >
-                    <ActionButton
-                      actionType='edit'
-                      postId={post.id}
-                      onClick={() => router.push(`/edit-post/${post.id}`)}
-                    />
-                    <ActionButton
-                      actionType='delete'
-                      postId={post.id}
-                      onClick={() => handleDelete(post.id)}
-                    />
+                    <div className='w-full'>
+                      <ActionButton
+                        onClick={() =>
+                          router.push(`/user/edit-post/${post?.id}`)
+                        }
+                        title={'Edit'}
+                      />
+                      <ActionButton
+                        isDisabled={post.status !== 'approve' ? false : true}
+                        onClick={() => handleApprovePost(post)}
+                        title={'Approve'}
+                      />
+                      <ActionButton
+                        isDisabled={post.status === 'deactivated' && true}
+                        onClick={() => handleDeactive(post)}
+                        title={'Deactivate'}
+                      />
+                      <ActionButton
+                        onClick={() => handleDelete(post)}
+                        title={'Delete'}
+                        isNormal={false}
+                      />
+                    </div>
                   </DynamicMenu>
                 </td>
               </tr>
             ))}
           </tbody>
+
+          {isRejectModalOpen && (
+            <RejectPostModal
+              isOpen={true}
+              onClose={() => setIsRejectModalOpen(false)}
+              postId={selectedPost.id}
+            />
+          )}
+          {isReviewModalOpen && (
+            <ReviewPostModal
+              isOpen={true}
+              onClose={() => setIsReviewModalOpen(false)}
+              // post={selectedPost}
+              id={selectedPost.id}
+            />
+          )}
+          {isConfirmModalOpen && (
+            <ConfirmModal
+              isOpen={true}
+              onClose={() => setIsConfirmModalOpen(false)}
+              onConfirm={handleConfirmAction}
+              action={confirmAction}
+            />
+          )}
         </table>
       </div>
-
       <Paggination
         currentPage={currentPage}
         totalPages={totalPages}
