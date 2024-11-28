f880f9efbc2d4f4fce8daf6b12861e53749c5a4e
dimpal001 <dimpaldas518@gmail.com>
2024-11-28 17:46:47 +0530

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,22 +1,40 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://thefashionsalad.com',
-  generateRobotsTxt: true,
-  exclude: ['/admin', '/private', '/user', '/api', '/context'],
+  generateRobotsTxt: true, // Generates the robots.txt file
+  exclude: [
+    '/admin',
+    '/private',
+    '/user',
+    '/api',
+    '/context',
+    '/_next/static/media/*',
+    '/cdn.thefashionsalad.com/*',
+  ],
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
-        disallow: ['/admin', '/private', '/user', '/api', '/context'],
+        disallow: [
+          '/admin',
+          '/private',
+          '/user',
+          '/api',
+          '/context',
+          '/_next/static/media/*',
+          '/cdn.thefashionsalad.com/*',
+        ],
       },
       {
         userAgent: '*',
         allow: [
+          '/about',
           '/contact',
           '/privacy-policy',
           '/terms-and-condition',
           '/favicon.ico',
           '/blogs/*',
+          '/category/*',
         ],
       },
     ],
